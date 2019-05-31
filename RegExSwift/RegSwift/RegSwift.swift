dae8ca0d0a1dbe6d5c2d5c29bb02a09c55a47af9
白烽 <white.zjh@alibaba-inc.com>
2019-05-31 15:06:36 +0800

    evolve

--- RegExSwift/RegSwift/RegSwift.swift
+++ RegExSwift/RegSwift/RegSwift.swift
@@ -9,13 +9,26 @@
 import Foundation
 
 class RegSwift {
-    
+    let headState: BaseState
     init(pattern: String) throws {
         let lexer = Lexer(pattern: pattern)
         let lexemes = try lexer.createLexemes()
         let parser = try Parser(lexemes: lexemes)
         let semanticUnits = try parser.getSemanticUnits()
         let headState = try StatesCreator.createHeadState(from: semanticUnits)
+        self.headState = headState
     }
     
+    func match(_ m: String) throws -> Bool {
+        guard !m.isEmpty else { throw RegExSwiftError("Error: Target string is empty") }
+        var evolve: [BaseState] = [headState]
+        for character in m {
+            evolve = evolve.flatMap { $0.forwardWithEmptyInput() }
+            evolve = evolve.flatMap { $0.forwardWithInput(character) }
+            if evolve.contains(where: { $0 === AcceptState.shared }) {
+                return true
+            }
+        }
+        return false
+    }
 }
